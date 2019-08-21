630f07c7b9b55b05fbb5dd4b727feee0512a95e2
raton <raton@marianaocean.com>
2019-08-21 13:43:19 +0900

    Vuex with progress Bar

---
+++ src/components/FilterTodos.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        Filter Todos:
+        <select @change="filterT($event)">
+            <option value="200">200</option>
+            <option value="100">100</option>
+            <option value="50">50</option>
+            <option value="20">20</option>
+            <option value="10">10</option>
+            <option value="5">5</option>
+        </select>
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+    name:'FilterTodos',
+    methods: {
+        ...mapActions(['filterTodos']),
+        filterT(event){
+            this.$bar.start()
+            this.filterTodos(event)
+                .then(() =>{
+                    this.$bar.finish()
+                })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import ProgressBar from './components/ProgressBar.vue'
+
+// global progress bar
+const bar = Vue.prototype.$bar = new Vue(ProgressBar).$mount()
+document.body.appendChild(bar.$el)
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
